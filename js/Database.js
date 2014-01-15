8b990dacc15fbd25f0fc91aa8f319d273ca744f1
David Rans <dave.g.rans@gmail.com>
2014-01-15 15:51:52 -0800

    renamed Web SQL wrapper, and added delete, selectAll methods

--- js/Database.js
+++ js/Database.js
@@ -17,7 +17,7 @@ Database.prototype.drop = function() {
 Database.prototype.create = function() {
 	this.db.transaction(function(tx) {
 		tx.executeSql(
-			'CREATE TABLE IF NOT EXISTS Devices ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, thumb TEXT )',
+			'CREATE TABLE IF NOT EXISTS Devices ( name TEXT, thumb TEXT )',
 			null,
 			function(transaction, result) { console.log(result); },
 			function(transaction, error) { console.log(error); }
@@ -28,8 +28,19 @@ Database.prototype.create = function() {
 Database.prototype.insert = function( name, thumb ) {
 	this.db.transaction(function(tx) {
 		tx.executeSql(
-			'INSERT INTO Devices (id, name, thumb) VALUES (?, ?, ?)',
-			[ null, name, thumb ],
+			'INSERT INTO Devices (name, thumb) VALUES (?, ?)',
+			[ name, thumb ],
+			function(transaction, result) { console.log(result); },
+			function(transaction, error) { console.log(error); }
+		);
+	});
+};
+
+Database.prototype.delete = function( name ) {
+	this.db.transaction(function(tx) {
+		tx.executeSql(
+			'DELETE FROM Devices WHERE name = ?',
+			[ name ],
 			function(transaction, result) { console.log(result); },
 			function(transaction, error) { console.log(error); }
 		);
@@ -46,6 +57,24 @@ Database.prototype.select = function( id ) {
 					var row = results.rows.item(i);
 					console.log( row );
 				}
+			},
+			function(transaction, error) { console.log(error); }
+		);
+	});
+};
+
+Database.prototype.selectAll = function( callback ) {
+	var rows = [];
+
+	this.db.transaction(function(tx) {
+		tx.executeSql(
+			'SELECT * FROM Devices',
+			null,
+			function(transaction, results) {
+				for( var i = 0; i < results.rows.length; i++ ) {
+					rows.push( results.rows.item(i) );
+				}
+				callback( results.rows.length, rows );
 			},
 			function(transaction, error) { console.log(error); }
 		);
